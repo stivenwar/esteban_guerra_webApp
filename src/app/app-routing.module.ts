81d970cb2e079554436f5cccd1daf2e79faefb4f
stivenwar <estebanguerra1990@gmail.com>
2023-05-16 11:26:50 +0200

    angular proyect Esteban Guerra

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import {AboutMeComponent} from "./components/about-me/about-me.component";
 import {CurriculumComponent} from "./components/curriculum/curriculum.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
   { path: 'about', component: AboutMeComponent},
   { path: 'home', component: HomeComponent},
   { path: 'cv', component: CurriculumComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
